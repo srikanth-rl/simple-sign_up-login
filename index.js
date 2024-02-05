9b76a430b9ee4baa0fb403de09c75b26827d9437
Srikanth2810 <lakshmi2003sri@gmail.com>
2024-02-05 23:45:45 +0530

    Fixed Email errors..

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static('Form'));
 
-const mongoURI = "mongodb://srikanth-rl:%23Srikanth2205@atlas-sql-65bf97f7eba2d94282cbe05b-get7d.a.query.mongodb.net/login-db?ssl=true&authSource=admin&directConnection=true";
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("DB connected..."))
-    .catch(err => console.error("Err in DB connection:", err));
+// mongoose.connect('mongodb://localhost:27017/users');
+const mongoURI = "mongodb+srv://srikanth-rl:%23Srikanth2205@cluster0.f6c7vle.mongodb.net/"
+mongoose.connect(mongoURI);
 
 const db = mongoose.connection;
+db.on('error', () => console.log("Err in DB connection..."));
+db.once('open', () => console.log("DB connected..."));
 
 const userSchema = new mongoose.Schema({
     name: String,
@@ -32,57 +32,58 @@ app.route('/login')
         res.sendFile(__dirname + '/Form/login.html');
     })
     .post(async(req, res) => {
-        try {
-            const email = req.body.email;
-            const password = req.body.password;
-            const user = await User.findOne({ email });
-            if (user) {
-                if (user.password === password) {
-                    console.log("User logged in..", user);
-                    return res.redirect('home.html');
-                } else {
-                    return res.status(401).send("Incorrect password. Enter the Right password...");
-                }
+        const email = req.body.email;
+        const password = req.body.password;
+        const user = await User.findOne({ email });
+        if (user) {
+            if (user.password === password) {
+                console.log("User logged in..", { name: user.name, phone_no: user.phone_no, email: user.email, password: user.password });
+                return res.redirect('home.html');
             } else {
-                console.log("User not registered");
-                return res.redirect('index.html');
+                return res.status(401).send("Incorrect password. Enter the Right password...");
             }
-        } catch (error) {
-            console.error("Error occurred:", error);
-            res.status(500).send("An error occurred while processing your request.");
+        } else {
+            console.log("User not registered");
+            return res.redirect('signup.html');
         }
     });
 
 app.post("/sign_up", async(req, res) => {
-    try {
-        const name = req.body.name;
-        const email = req.body.email;
-        const phno = req.body.phno;
-        const pwd = req.body.password;
-        const rePwd = req.body.rePassword;
+    const name = req.body.name;
+    const email = req.body.email;
+    const phno = req.body.phno;
+    const pwd = req.body.password;
+    const rePwd = req.body.rePassword;
 
-        if (pwd !== rePwd) {
-            return res.redirect('/');
-        }
+    if (pwd !== rePwd) {
+        return res.redirect('/');
+    }
 
-        const existingUser = await User.findOne({ email: email });
-        if (existingUser) {
-            return res.status(400).send("Sorry, your Email id already exists.");
-        }
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+        return res.status(400).send("Sorry, your Email id already exists.");
+    }
 
-        const newUser = new User({
-            name: name,
-            phone_no: phno,
-            email: email,
-            password: pwd
-        });
+    const newUser = new User({
+        name: name,
+        phone_no: phno,
+        email: email,
+        password: pwd
+    });
 
-        await newUser.save();
-        console.log("Record stored successfully..");
-        return res.redirect('login.html');
-    } catch (error) {
-        console.error("Error occurred:", error);
-        res.status(500).send("An error occurred while processing your request.");
+    await newUser.save();
+    console.log("Record stored successfully..");
+    return res.redirect('login.html');
+});
+
+// New route to handle email availability check
+app.post("/checkEmail", async(req, res) => {
+    const { email } = req.body;
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+        res.json({ exists: true });
+    } else {
+        res.json({ exists: false });
     }
 });
 
@@ -90,8 +91,7 @@ app.get("/", (req, res) => {
     res.set({
         "Access-Control-Allow-Origin": "*"
     });
-    return res.redirect('index.html');
+    return res.redirect('signup.html');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000);
